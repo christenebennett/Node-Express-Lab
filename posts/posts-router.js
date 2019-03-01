a127dd5048e28450544ecce23ccc066311ad2568
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-01 09:48:12 -0700

    Add POST to /api/posts endpoint

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -23,6 +23,20 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+  try {
+    const newPost = req.body;
+    if (newPost.title && newPost.contents) {
+      const post = await Posts.insert(newPost);
+      res.status(201).json({post});
+    } else {
+      res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "There was an error while saving the post to the database"})
+  }
+})
+
 
 
 module.exports = router;
